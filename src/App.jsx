3edd93f5a8a9957d05ef3a383dd45c8bc0e81752
SriVarshininoothi <noothi.srivarshini982@gmail.com>
2026-07-30 14:28:12 +0530

    RouterDom

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
-import Props from './JSX_Props/Props'
-import Rendering from './JSX_Props/Rendering'
-import Counter from './useState - ReactHook/counter'
-import ObjectState from './useState - ReactHook/ObjectState'
-import ArrayState from './useState - ReactHook/ArrayState'
-import FormInput from './useState - ReactHook/FormInput'
-import Product from './JSX_Props/Product'
-import Rendering_list from './Rendering_Lists/Rendering_list'
-import FormValidation from './Events&FormHandling/FormValidation'
-import FetchAPI from './UseEffect/FetchAPI'
-import ProductCards from './UseEffect/ProductCards'
-import RandomuserGenerator from './UseEffect/RandomuserGenertor'
-import EmployeeDashboard from './Rendering_Lists/EmployeeDashboard'
+
+import Header from './Router/Components/Header'
+
+import { Routes, Route } from 'react-router-dom'
+
+import Home from './Router/Pages/Home'
+import About from './Router/Pages/About'
+import Contact from './Router/Pages/Contact'
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
 
-       {/* <Props /> 
-       <Rendering/> */}
-      {/* <Counter/>  */}
-      {/* <ObjectState/> */}
-      {/* <ArrayState/> */}
-      {/* <FormInput/> */}
-      {/* <Product/> */}
-      {/* <Rendering_list/> */}
-      {/* <FormValidation/> */}
-      {/* <FetchAPI/> */}
-      {/* <ProductCards/> */}
+      <Header />
 
-      {/* <RandomuserGenerator/> */}
-      <EmployeeDashboard/>
-      
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path='/About' element={<About />} />
+
+<Route path='/Contact' element={<Contact/>}/>
+      </Routes>
+
+
+
     </>
   )
 }
---
+++ src/Router/Pages/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+
+
+    return(
+        <div>
+            <h1>Welcome to About</h1>
+        </div>
+    )
+
+
+}
+export default About;
